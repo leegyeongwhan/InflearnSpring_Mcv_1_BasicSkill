b3f795731720cfdaf1b8a14fac8b3125a54aa500
leegyeongwhan <leekhy02@naver.com>
2022-08-23 16:45:36 +0900

    feat: 인프런 스프링 mvc1편 http 요청메세지- 단순텍스트 예제

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,25 +1,117 @@
 package hello.springmvc.basic.request;
 
+import hello.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
+@RestController
 @Controller
 public class RequestParamController {
 
     @RequestMapping("/request-param-v1")
     public void requestParamV1(HttpServletRequest request
-    , HttpServletResponse response) throws IOException {
+            , HttpServletResponse response) throws IOException {
         String name = request.getParameter("name");
         int age = Integer.parseInt(request.getParameter("age"));
 
         log.info("username={}, age={}", name, age);
         response.getWriter().write("ok");
 
+    }
+
+
+    @ResponseBody //없을경우 controller어노테이션떄문에 메서드 반화나입이
+    //String이여서 viewResovled 를 호출하게되는데 이어노테이션을써
+    //그냥 ok를 반환하자
+    @RequestMapping("/request-param-v3")
+    public String requestParamV2(
+            @RequestParam String username,
+            @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+
+    /**
+     * @RequestParam 사용
+     * String, int 등의 단순 타입이면 @RequestParam 도 생략 가능
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam.required /request-param-required -> username이 없으므로 예외
+     * <p>
+     * 주의!
+     * /request-param-required?username= -> 빈문자로 통과
+     * <p>
+     * 주의!
+     * /request-param-required
+     * int age -> null을 int에 입력하는 것은 불가능, 따라서 Integer 변경해야 함(또는 다음에 나오는
+     * defaultValue 사용)
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam Map, MultiValueMap
+     * Map(key=value)
+     * MultiValueMap(key=[value1, value2, ...]) ex) (key=userIds, value=[id1, id2])
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"),
+                paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(
+            @ModelAttribute HelloData helloData
+  ){
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+
+        return "ok";
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(
+             HelloData helloData
+    ){
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+
+        return "ok";
     }
 }
